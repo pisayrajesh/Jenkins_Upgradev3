4891b8cb8a31537839fb491fff819338bdb45180
Rajesh <pishwarajesh@gmail.com>
2020-12-31 21:35:45 +0530

    saved the changes over Tomcat tutorial

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
   @Test
   public void greetShouldIncludeMeetingMessage() {
-    String someone = "Anshul, Thanks you for your great work";
+    // String someone = "Anshul, Thanks you for your great work";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
